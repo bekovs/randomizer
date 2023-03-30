7ef6922c7c77f8b653aa07730a0d73ef8b4c964b
sultan bekov <bekovs96@gmail.com>
2023-03-30 12:09:49 +0600

    .

---
+++ main.js
@@ -0,0 +1,58 @@
+const names = document.querySelector('.names');
+const runBtn = document.querySelector('.run-btn');
+const timeToSelect = 3000;
+
+const namesArr = ["Шарипов Джумар", "Нурбек у. Бекназар", "Болотова Мээрим", "Расторгуев Владислав", "Шайназаров Олжобай", "Нурдинов Азамат", "Ибраимов Айдарбек", "Жайлообеков Нурбол", "Абдылдаев Айдар", "Курбанкулов Ислам", "Каскеева Каныкей", "Курманалиев Нурбек", "Карасартов Азамат", "Кадыров Мирбек", "Максатбеков Адилет", "Кожомуратов Эржан", "Аскарбекова Магира", "Абакиров Баястан", "Жылдызбек у. Актилек", "Асанов Акжол"];
+
+const render = () => {
+  names.innerHTML = ``;
+  namesArr.forEach((name) => {
+    names.innerHTML += `
+    <p class="name">${name}</p>
+    ` 
+  })
+}
+
+render()
+
+let nameBlocks = document.querySelectorAll('.name');
+
+const randomStudentIndex = (arr = namesArr) => {
+  return Math.floor(Math.random() * arr.length);
+}
+
+const randomizeAction = () => {
+  let colorize = setInterval(() => {
+    let randIndex = Math.floor(Math.random() * nameBlocks.length);
+    nameBlocks[randIndex].style.backgroundColor = 'rgb(255, 192, 184)';
+    setTimeout(() => {
+      nameBlocks[randIndex].style.backgroundColor = 'white';
+    }, 150)
+  }, 150)
+  setTimeout(() => {
+    clearInterval(colorize);
+  }, timeToSelect)
+}
+
+const runRandomize = () => {
+  let winner = randomStudentIndex();
+  randomizeAction();
+  setTimeout(() => {
+    let pointing = setInterval(() => {
+      nameBlocks[winner].style.backgroundColor = 'white';
+      setTimeout(() => {
+        nameBlocks[winner].style.backgroundColor = 'rgb(255, 192, 184)';
+      }, 50)
+    }, 150)
+    setTimeout(() => {
+      clearInterval(pointing);
+    }, 2000);
+  }, timeToSelect)
+}
+
+runBtn.addEventListener('click', () => {
+  nameBlocks.forEach((name) => {
+    name.style.backgroundColor = 'white';
+  })
+  runRandomize();
+})
